e75c5c45553a9079ba96b58d46693363247e0567
Akmal Ismoilov <master80098@gmail.com>
2022-08-25 16:40:36 +0500

    Task completed!

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,42 @@
-package com.epam.mjc.nio;
+package com.epam.mjc.io;
+
+import com.epam.mjc.nio.Profile;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 
+public class FileReader
+{
 
-public class FileReader {
+    public Profile getDataFromFile(File file)
+    {
+        Profile profile = null;
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+        try (InputStream inputStream = new FileInputStream(file))
+        {
+            if(!Files.exists(file.toPath())) {
+                throw new IOException("File not found");
+            }
+            //Read file and save lines
+            String fileContent = new String(inputStream.readAllBytes());
+            String[] lines = fileContent.split("\\R");
+
+            //Parse lines and get the data
+            String name = lines[0].split(" ")[1];
+            Integer age = Integer.parseInt(lines[1].split(" ")[1]);
+            String email = lines[2].split(" ")[1];
+            Long phone = Long.parseLong(lines[3].split(" ")[1]);
+
+            //Create profile
+            profile = new Profile(name, age, email, phone);
+        } catch (IOException e)
+        {
+            //Some exception handling
+        }
+
+        return profile;
     }
 }
